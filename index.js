020b256a8db06e79519c1e79deda66a5f20b3340
Shavu parmar <129254310+shavuparmar@users.noreply.github.com>
2024-07-15 21:04:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+let sign = document.getElementById("SignIn");
+sign.addEventListener("click",Signin);
+
+function Signin(){
+    window.location.href="#"
+}
+
+
+let Getstarted = document.getElementById("GetStarted");
+Getstarted.addEventListener("click",Getstarteds);
+function Getstarteds(){
+     alert("This site is Under Process!! Try again after sometime!!")
+}
+
